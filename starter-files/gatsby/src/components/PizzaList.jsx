f41997423aab535b7e56f8eb586af264afa24f33
Philly Stafford <phillystafford@gmail.com>
2020-11-06 00:29:32 +0000

    M5-03 - Learning Gatsby Queries

---
+++ starter-files/gatsby/src/components/PizzaList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+function SinglePizza({ pizza }) {
+  return (
+    <>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      </Link>
+    </>
+  );
+}
+
+export default function PizzaList({ pizzas }) {
+  return (
+    <>
+      {pizzas.map((pizza) => (
+        <SinglePizza pizza={pizza} key={pizza.id} />
+      ))}
+    </>
+  );
+}
--- starter-files/gatsby/src/pages/pizzas.jsx
+++ starter-files/gatsby/src/pages/pizzas.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage(props) {
+export default function PizzasPage({ data }) {
+  console.log('PizzasPage -> data', data);
   return (
     <>
-      <h1>Im the pizzas page</h1>
+      <PizzaList pizzas={data.pizzas.nodes} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
