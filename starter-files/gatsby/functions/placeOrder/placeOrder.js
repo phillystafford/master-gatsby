6e679a251ceb6f0b669a5f49015d0bf4e510f0c3
Philly Stafford <phillystafford@gmail.com>
2020-12-08 23:10:01 +0000

    M11-41 - Setting Error, Loading and Success States

--- starter-files/gatsby/functions/placeOrder/placeOrder.js
+++ starter-files/gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,30 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `
+  <div>
+    <h2>Your Recent order for ${total}</h2>
+    <p>Will be ready in 10 minutes</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `
+      <li>
+        <img src="${item.thumbnail}" alt="${item.name}"/>
+        ${item.size} ${item.name} - ${item.price}
+      </li>`
+        )
+        .join('')}
+    </ul>
+    <p>Your total is <strong>${total}</strong> due at pickup</p>
+    <style>
+      ul {
+        list-style-type: none;
+      }
+    </style>
+  </div>`;
+}
+
 // 1. create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,20 +36,47 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  console.log(
+    '🚀 ~ file: placeOrder.js ~ line 14 ~ exports.handler= ~ event.body',
+    event.body
+  );
+  const body = JSON.parse(event.body);
+  console.log(
+    '🚀 ~ file: placeOrder.js ~ line 19 ~ exports.handler= ~ body',
+    body
+  );
+  // validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    console.log(`Checking field: -> `, field);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Bummer, you're missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // send the email
+  // send the success or error message
+
   // test send an email
   const info = await transporter.sendMail({
     from: 'Slicks Slices <slick@example.com>',
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New order!',
-    html: `<p>Your new pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
-
   console.log(
-    '🚀 ~ file: placeOrder.js ~ line 25 ~ exports.handler= ~ info',
+    '🚀 ~ file: placeOrder.js ~ line 31 ~ exports.handler= ~ info',
     info
   );
+
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success 😃' }),
   };
 };
