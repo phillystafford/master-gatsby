cb8264717d1d34cc0e12958f2e60ddf519ab84ef
Philly Stafford <phillystafford@gmail.com>
2020-12-11 23:24:21 +0000

    M11-44 - Creating a one off Store settings page

---
+++ starter-files/sanity/sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import S from '@sanity/desk-tool/structure-builder';
+
+// build a custom sidebar
+export default function Sidebar() {
+  return S.list()
+    .title(`Slick's Slices`)
+    .items([
+      // create new sub item
+      S.listItem()
+        .title('Home Page')
+        .icon(() => <strong>🔥</strong>)
+        // make a new document id so we don't have a string of numbers
+        .child(S.editor().schemaType('storeSettings').documentId('downtown')),
+      // add in the rest of our document items
+      ...S.documentTypeListItems().filter(
+        (item) => item.getId() !== 'storeSettings'
+      ),
+    ]);
+}
