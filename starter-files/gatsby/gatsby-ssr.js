23160fdb8e2b9e78985feab503463f63210225ab
Philly Stafford <phillystafford@gmail.com>
2020-12-17 23:57:04 +0000

    M13-49 - Building our gatsby site

--- starter-files/gatsby/gatsby-ssr.js
+++ starter-files/gatsby/gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import { OrderProvider } from './src/components/OrderContext';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
